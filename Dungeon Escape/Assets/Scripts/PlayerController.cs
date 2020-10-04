959de4964a45b6c8da1b7bbdcfd673e2ee1bbd75
rinkerm <rinkerm915@gmail.com>
2020-10-04 01:41:26 -0700

    Moving parts

---
+++ Dungeon Escape/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D rb;
+    [SerializeField] float jumpforce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.GetInteger("State") == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                anim.SetInteger("State",1);
+                jump();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                anim.SetInteger("State", 2);
+            }
+        }
+        else
+        {
+            if (!AnimatorIsPlaying())
+            {
+                anim.SetInteger("State", 0);
+            }
+        }
+    }
+    bool AnimatorIsPlaying()
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1;
+    }
+    void jump()
+    {
+        rb.velocity = new Vector2(0, jumpforce);
+    }
+}
